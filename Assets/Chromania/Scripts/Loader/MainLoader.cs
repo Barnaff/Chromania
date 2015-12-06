ae1dd17a4b360cb2ef684c2b858ba3686eabf0f2
Barnaff <kobi.chariski@gmail.com>
2015-12-06 23:31:18 +0200

    Added parse framework

---
+++ Assets/Chromania/Scripts/Loader/MainLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainLoader : MonoBehaviour {
+
+    #region Privte Properties
+
+    [SerializeField]
+    private string _version;
+
+    #endregion
+
+
+    #region Initialize
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(LoadGameCorutine());
+	}
+
+    #endregion
+
+
+    #region Private
+
+    IEnumerator LoadGameCorutine()
+    {
+        Debug.Log("start loading game");
+        
+        yield return null;
+    }
+
+    #endregion
+}
