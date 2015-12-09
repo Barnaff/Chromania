4772ac6cfeb3774bcf76739bf05146bedb27ed17
Barnaff <kobi.chariski@gmail.com>
2015-12-09 22:31:07 +0200

    Upgraded to unity 5.3

--- Assets/Chromania/Scripts/Loader/MainLoader.cs
+++ Assets/Chromania/Scripts/Loader/MainLoader.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class MainLoader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    #region Private Properties
+
+    [SerializeField]
+    private string _version;
+
+    [SerializeField]
+    private GameObject _loaderController;
+
+    #endregion
+
+
+    #region Initialize
+
+    // Use this for initialization
+    void Start () {
+
+
+        StartCoroutine(GameInitializationCorutine());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    #endregion
+
+ 
+
+    #region Private
+
+    IEnumerator GameInitializationCorutine()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        GameObject mainMenuControllerPrefab = Resources.Load("MainMenuController") as GameObject;
+        if (mainMenuControllerPrefab != null)
+        {
+            GameObject mainMenuController = Instantiate(mainMenuControllerPrefab) as GameObject;
+            mainMenuController.transform.position = Vector3.zero;
+        }
+
+        if (_loaderController != null)
+        {
+            Destroy(_loaderController);
+        }
+    }
+
+    #endregion
 }
