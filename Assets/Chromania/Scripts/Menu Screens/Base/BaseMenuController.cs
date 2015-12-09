c521ad369a76ae364713f8e138559c5c1eb9a050
Barnaff <kobi.chariski@gmail.com>
2015-12-10 00:57:28 +0200

    Added screens base controller

---
+++ Assets/Chromania/Scripts/Menu Screens/Base/BaseMenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseMenuController : MonoBehaviour {
+
+    private Animator _animator;
+
+    // Use this for initialization
+    void Start () {
+        _animator = this.gameObject.GetComponent<Animator>();
+        if (_animator != null)
+        {
+            Debug.LogError("ERROR - Menu screen missing animaotr controller");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	public virtual void OnEnterAnimationComplete() {}
+
+	public virtual void OnExitAnimationComplete() {}
+
+}
---
+++ Assets/Chromania/Scripts/Menu Screens/Main Menu/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MainMenuController : BaseMenuController {
+
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void OnEnterAnimationComplete ()
+	{
+		Debug.Log("Enter animation complete");
+	}
+
+    public override void OnExitAnimationComplete()
+    {
+        throw new NotImplementedException();
+    }
+}
