2a2b30fc72811197ce6e660858af12bc4306203c
Barnaff <kobi.chariski@gmail.com>
2016-02-20 19:54:45 +0200

    Added back button manager

---
+++ Assets/Chromania/Components Factory/Components/Back Button/BackButtonManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BackButtonManager : MonoBehaviour , IBackButton {
+
+	private	IPopups _popupsManager;
+
+	private List<OnBackButtonMethod> _responders = new List<OnBackButtonMethod>();
+
+	void Start()
+	{
+		_popupsManager = ComponentFactory.GetAComponent<IPopups>() as IPopups;
+	}
+
+	void Update()
+	{ 
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Back();
+		}
+	}
+
+	public void RegisterToBackButton(OnBackButtonMethod callBack)
+	{
+		if (_responders.Contains(callBack))
+		{
+			_responders.Remove(callBack);
+		}
+		_responders.Add(callBack);
+
+	}
+
+	public void RemoveResponderFromBackButton(OnBackButtonMethod callBack)
+	{
+		if (_responders.Contains(callBack))
+		{
+			_responders.Remove(callBack);
+		}
+	}
+
+	private void Back()
+	{
+		if (_popupsManager != null && _popupsManager.IsDisplayingPopup())
+		{
+			PopupBaseController popupBaseController =  _popupsManager.CurrentActivePopup();
+			if (popupBaseController != null)
+			{
+				popupBaseController.BackButtonPressed();
+			}
+			return;
+		}
+
+		if (_responders.Count > 0)
+		{
+			OnBackButtonMethod callback = _responders[_responders.Count - 1];
+			if (callback != null)
+			{
+				callback();
+			}
+		}
+	}
+
+}
