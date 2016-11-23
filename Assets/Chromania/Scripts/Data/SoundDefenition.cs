db37251de15b7f323958624cafb67ae6d1f3655a
Barnaff <kobi.chariski@gmail.com>
2016-11-23 03:19:39 +0200

    Added sound manager and gameplay sounds

---
+++ Assets/Chromania/Scripts/Data/SoundDefenition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDefenition : ScriptableObject {
+
+    public AudioClip AudioClip;
+
+    public float MinVolume = 1.0f;
+    public float MaxVolume = 1.0f;
+
+    public float MinPitch = 1.0f;
+    public float MaxPitch = 1.0f;
+
+    public bool Looping = false;
+
+    public float Delay = 0f;
+
+    public void Play()
+    {
+        SoundManager.PlaySFX(AudioClip, Looping, Delay, Random.Range(MinVolume, MaxVolume), Random.Range(MinPitch, MaxPitch));
+    }
+}
---
+++ Assets/Chromania/Scripts/Gameplay/Gameplay Managers/GameplaySoundManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplaySoundManager : MonoBehaviour {
+
+    [SerializeField]
+    private SoundDefenition _chromieSpwaned;
+
+    [SerializeField]
+    private SoundDefenition _chromieCollected;
+
+    [SerializeField]
+    private SoundDefenition _chromieDropped;
+
+    // Use this for initialization
+    void Start () {
+
+        GameplayEventsDispatcher.Instance.OnChromieSpawned += (chromieController) =>
+        {
+            PlaySound(_chromieSpwaned);
+        };
+
+        GameplayEventsDispatcher.Instance.OnChromieCollected += (chromieController, colorZone) =>
+        {
+            PlaySound(_chromieCollected);
+        };
+
+        GameplayEventsDispatcher.Instance.OnLivesUpdate += (maxLives, currentLives, change) =>
+        {
+            if (change < 0)
+            {
+                PlaySound(_chromieDropped);
+            }
+        };
+    }
+
+
+    private void PlaySound(SoundDefenition soundDefenition)
+    {
+        soundDefenition.Play();
+    }
+
+}
