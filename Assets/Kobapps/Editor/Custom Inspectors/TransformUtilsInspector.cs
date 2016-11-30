d61a7758213bd6d749e0e8e6c5b4b75fd5635813
Barnaff <kobi.chariski@gmail.com>
2016-11-30 02:33:10 +0200

    Updated Edtior scripts

---
+++ Assets/Kobapps/Editor/Custom Inspectors/TransformUtilsInspector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(Transform), true)]
+public class TransformInspector : UnityEditor.Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Reset"))
+        {
+            
+        }   
+    }
+}
